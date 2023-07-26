5b92665ad2e536779270177e528e0038d49412fe
Diego Almeida <dyhalmeida@gmail.com>
2023-07-26 10:16:34 -0300

    feat: add update student handler

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -47,14 +49,79 @@ func createStudentsHandler(ctx *gin.Context) {
 	})
 }
 
+func updateStudentsHandler(ctx *gin.Context) {
+	ctx.Header("Content-type", "application/json")
+
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": fmt.Errorf("param: %d (type: %s) is required", id, "router parameter"),
+		})
+		return
+	}
+
+	var student Student
+	if err := ctx.BindJSON(&student); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "could not get payload",
+		})
+		return
+	}
+
+	if student.Fullname == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "fullname is required",
+		})
+		return
+	}
+
+	if student.Age <= 0 {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "age is required",
+		})
+		return
+	}
+
+	var studentFound *Student
+	for _, s := range Students {
+		if s.ID == id {
+			studentFound = &s
+			break
+		}
+	}
+
+	if studentFound == nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"message": "student with id: " + strconv.Itoa(id) + " not found",
+		})
+		return
+	}
+
+	studentFound.Fullname = student.Fullname
+	studentFound.Age = student.Age
+
+	for i, s := range Students {
+		if studentFound.ID == s.ID {
+			Students[i].Fullname = studentFound.Fullname
+			Students[i].Age = studentFound.Age
+			break
+		}
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": studentFound,
+	})
+
+}
+
 func getRoutes(gin *gin.Engine) *gin.Engine {
 	gin.GET("/heart", hearthHandler)
 	studentsGroupV1 := gin.Group("/api/v1")
 	{
 		studentsGroupV1.GET("/student", listStudentsHandler)
 		studentsGroupV1.POST("/student", createStudentsHandler)
+		studentsGroupV1.PUT("/student/:id", updateStudentsHandler)
 	}
-
 	return gin
 }
 
