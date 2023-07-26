ce55277f10d559df0a77162dfa44fd6f20c55045
Diego Almeida <dyhalmeida@gmail.com>
2023-07-26 09:13:31 -0300

    feat: add list student handler

--- main.go
+++ main.go
@@ -6,6 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Student struct {
+	ID       int    `json:"id"`
+	Fullname string `json:"fullname"`
+	Age      int64  `json:"age"`
+}
+
+var Students = []Student{
+	Student{ID: 1, Fullname: "Diego", Age: 31},
+	Student{ID: 2, Fullname: "Mayara", Age: 28},
+}
+
 func hearthHandler(ctx *gin.Context) {
 	ctx.Header("Content-type", "application/json")
 	ctx.JSON(http.StatusOK, gin.H{
@@ -13,8 +24,20 @@ func hearthHandler(ctx *gin.Context) {
 	})
 }
 
+func listStudentsHandler(ctx *gin.Context) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": Students,
+	})
+}
+
 func getRoutes(gin *gin.Engine) *gin.Engine {
 	gin.GET("/heart", hearthHandler)
+	studentsGroupV1 := gin.Group("/api/v1")
+	{
+		studentsGroupV1.GET("/student", listStudentsHandler)
+	}
+
 	return gin
 }
 
